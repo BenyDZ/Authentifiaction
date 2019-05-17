f4cfd66f716bd4a9cd1836dc34b36f11d752c1a3
BenyDZ <patdenzoungany@gmail.com>
2019-05-17 14:02:15 +0200

    merge with Brendy

--- js/Octopus/octopus.js
+++ js/Octopus/octopus.js
@@ -1,1 +1,15 @@
-function signIn()
+var octopus = {
+    register : function(name, password, role){
+        
+    const fs = require('fs');
+  
+    // Data which will write in a file. 
+    data = ""+name+"\n"+password+"\n";
+  
+    // Write data in 'Output.txt' . 
+    fs.writeFile(role+'.txt', data, (err) => { 
+      
+        // In case of a error throw err. 
+        if (err) throw err; 
+    }) }
+}
---
+++ js/View/view.js
@@ -0,0 +1,8 @@
+var view = {
+    getUserInfo : function(){
+        var name = document.getElementById('name').value;
+        var password = document.getElementById('password').value;
+        var role = document.getElementsByTagName('section').value;
+        
+    }
+}

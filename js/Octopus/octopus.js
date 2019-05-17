985db8f827cb8c0785ac1a5dcd7153fa67db62da
unknown <nyameyhilary@gmail.com>
2019-05-17 16:20:17 +0200

    add 3 views

--- js/Octopus/octopus.js
+++ js/Octopus/octopus.js
@@ -1,19 +1,15 @@
 var OCTOPUS={
-    readJSON :function(path) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', path, true);
-        xhr.responseType = 'blob';
-        xhr.onload = function(e) { 
-            if (this.status == 200) {
-                var file = new File([this.response], 'temp');
-                var fileReader = new FileReader();
-                fileReader.addEventListener('load', function(){
-                   //do stuff with fileReader.result
-                });
-                fileReader.readAsText(file);
+    readTextFile: function(){
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET","D:/PANESS IIHT/COURS/GROUP WORK/Projet 2/Authentifiaction/Save/Utilisateur.txt","true");
+        rawFile.onreadystatechange = function (){
+            if(rawFile.readyState == 4 && rawFile.status == 200 ){
+                document.getElementById("Name").innerHTML = rawFile.responseText;
+                document.getElementById("password").innerHTML = rawFile.responseText;
             } 
-        }
-        xhr.send();
+        };
+        rawFile.send();
+        console.log(rawFile);
     }
 }
-OCTOPUS.readJSON();
+OCTOPUS.readTextFile();
